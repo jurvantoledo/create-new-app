2c2a374864cb357ec3d7097509cf060447a57468
jurvantoledo <jurvantoledo@live.nl>
2020-10-15 16:02:54 +0200

    made NavBar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
+// Peinemann.nl
+import { Switch, Route, Link } from "react-router-dom";
 import React from "react";
 import "./App.css";
+import AboutPage from "./pages/AboutPage";
+import DiscoverMoviesPage from "./pages/DiscoverMoviesPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <Link to="discover">Discover Movies</Link>;
+        <Link to="/about">About this page</Link>;<Link to="/">Homepage</Link>;
+      </nav>
+      <Switch>
+        <Route path="/discover" component={DiscoverMoviesPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/DiscoverMoviesPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DiscoverMoviesPage(props) {
+  return [props.DiscoverMoviesPage];
+}
